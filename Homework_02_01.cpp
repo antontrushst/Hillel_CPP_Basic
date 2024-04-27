f4cfd60ad3a6e62806d26565748884b81a9272ab
Anton Trush <anton.st.trush@gmail.com>
2024-04-27 15:59:45 +0300

    Add 2nd homework files

---
+++ Homework_02_01.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+// Ця програма відображає повідомлення "I love C++!"
+int main()
+{
+    std::cout << "I love C++!\n";
+    return 0;
+}
---
+++ Homework_02_04.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+// Виправлена версія коду з 3-го завдання.
+int main()
+{
+  cout << "Is there a bug here?";
+  return 0;
+}    
